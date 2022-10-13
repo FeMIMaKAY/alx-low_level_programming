b9cde0eaed2c5499b50cadf3084c09d48d2301a6
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-13 04:13:20 -0700

    C task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-  * main - prints the phrase 
-  * Return: 0 if exited properly, non-zero otherwise
-  */
+ * main - prints the phrase 
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");			return (0);
