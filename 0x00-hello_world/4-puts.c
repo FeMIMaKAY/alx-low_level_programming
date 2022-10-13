7fa0a66d4a7ae9f3631b106c9a9dfa6d3fa7fbfc
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-13 04:10:51 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - prints the phrase "Programming is like building a multilingual puzzle
+  * main - prints the phrase 
   * Return: 0 if exited properly, non-zero otherwise
   */
 int main(void)
