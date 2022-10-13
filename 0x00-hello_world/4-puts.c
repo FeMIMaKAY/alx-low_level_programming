35ac1bbadd4b29187debb7edb4eeda632e00be97
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-13 04:23:57 -0700

    C programs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints the phrase 
- * Return: 0 if exited properly, non-zero otherwise
+ * Return:if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
