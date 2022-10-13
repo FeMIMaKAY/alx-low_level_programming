9e17bf775e2be9006dbf84a445034932d15b5d11
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-13 03:37:15 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 
 /**
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *   * Return: 0 if exited properly, non-zero otherwise
- *    */
+   * main - prints the phrase "Programming is like building a multilingual puzzle
+    * Return: 0 if exited properly, non-zero otherwise
+    */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
 }
