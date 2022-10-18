cfcd61370a51b27266aa543dab6b040e0398b932
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-18 21:25:38 +0100

    task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,49 +1,23 @@
 #include "main.h"
-
 /**
- * jack_bauer - prints every minute of the day
- *
- * Description: prints every minute of the day
- *
- * Return: void
- */
-
+*jack_bauer - prints every minute of the day starting from 00:00 to 23:59
+*
+* Return: returns 0
+*/
 void jack_bauer(void)
 {
-	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
-u
-	hours_max = 58;
-	hours_tens = '0';
-	while (hours_tens < '3')
-	{
-	if (hours_tens == '2')
-	{
-	hours_max = '4';
-	}
-	hours_ones = '0';
-	while (hours_ones < hours_max)
-	{
-	minutes_tens = '0';
-	while (minutes_tens < '6')
-	{
-	minutes_ones = '0';
-	while (minutes_ones < 58)
-	{
-	_putchar(hours_tens);
-	_putchar(hours_ones);
-	_putchar(':');
-	_putchar(minutes_tens);
-	_putchar(minutes_ones);
-	_putchar('\n');
-	minutes_ones++;
-	}
-	minutes_ones = '0';
-	minutes_tens++;
-	}
-	minutes_tens = '0';
-	hours_ones++;
-	}
-	hours_ones = '0';
-	hours_tens++;
-	}
+int hr;
+int min;
+for (hr = 0; hr < 24; hr++)
+{
+for (min = 0; min < 60; min++)
+{
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
+}
+}
 }
