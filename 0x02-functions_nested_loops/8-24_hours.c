e90f01bf3668d4027861a66f627c44954bffd8a1
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-18 13:27:26 -0700

    function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,14 @@ int hr;
 int min;
 for (hr = 0; hr < 24; hr++)
 {
-for (min = 0; min < 60; min++)
+	for (min = 0; min < 60; min++)
 {
-_putchar((hr / 10) + '0');
-_putchar((hr % 10) + '0');
-_putchar(':');
-_putchar((min / 10) + '0');
-_putchar((min % 10) + '0');
-_putchar('\n');
+	_putchar((hr / 10) + '0');
+	_putchar((hr % 10) + '0');
+	_putchar(':');
+	_putchar((min / 10) + '0');
+	_putchar((min % 10) + '0');
+	_putchar('\n');
 }
 }
 }
