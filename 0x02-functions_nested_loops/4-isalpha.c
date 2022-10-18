21234aa3b74dc6f57cdee1d8002c86ede299794b
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-18 11:45:25 -0700

    function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,14 +11,14 @@ int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return (1);
+	return (1);
 	}
 	else if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
 }
