40050c42bb50ce3fb31298bae165fa66e8354e8e
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-20 09:15:27 -0700

    debugging so that it correctly prints out the largest of three numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
+ * largest_number- - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
