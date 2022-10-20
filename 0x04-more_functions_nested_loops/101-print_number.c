506eeea7edc4ed382d633a77c56c430c2678bc85
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-20 12:27:29 -0700

    program that finds and prints the largest prime factor of the number 612852475143

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,12 +11,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+	_putchar(45);
+	m = n * -1;
 	}
 	else
 	{
-		m = n;
+	m = n;
 	}
 
 	d = m;
@@ -24,12 +24,12 @@ void print_number(int n)
 
 	while (d > 9)
 	{
-		d /= 10;
-		count *= 10;
+	d /= 10;
+	count *= 10;
 	}
 
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / count) % 10) + 48);
+	_putchar(((m / count) % 10) + 48);
 	}
 }
