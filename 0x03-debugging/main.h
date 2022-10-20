57547567fa3cee55e3d2cc7b47cd8e48c34d3a89
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-20 09:03:34 -0700

    main.h config

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+	void positive_or_negative(int i);
+	int largest_number(int a, int b, int c);
+	int convert_day(int month, int day);
+	void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
