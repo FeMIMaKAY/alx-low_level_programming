170b5850910c08eec66e13c9728fe411dd9ad9f0
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-20 17:00:35 +0100

    task

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
