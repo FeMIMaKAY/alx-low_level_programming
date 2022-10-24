f7be379b5a28d417cdc96bc6c2d492a8cac7fc1f
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-24 12:15:29 -0700

    function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,25 +11,25 @@ int _atoi(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
+	if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+	break;
 
-		if (*(s + count) == '-')
-			pn *= -1;
+	if (*(s + count) == '-')
+	pn *= -1;
 
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-		{
-			if (size > 0)
-				m *= 10;
-			size++;
-		}
-		count++;
+	if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+	{
+	if (size > 0)
+	m *= 10;
+	size++;
+	}
+	count++;
 	}
 
 	for (i = count - size; i < count; i++)
 	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
+	oi = oi + ((*(s + i) - 48) * m);
+	m /= 10;
 	}
 	return (oi * pn);
 }
