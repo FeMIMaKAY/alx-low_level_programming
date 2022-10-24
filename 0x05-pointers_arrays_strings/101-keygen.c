dbad26f554f094d5fbfc4137c235e3686327236c
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-24 12:16:43 -0700

    program that generates random valid passwords for the program 101-crackme.

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,11 +13,11 @@ int main(void)
 	srand((unsigned int) time(&t));
 	while (c < 2772)
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
+	r = rand() % 128;
+	if ((c + r) > 2772)
+	break;
+	c = c + r;
+	printf("%c", r);
 	}
 	printf("%c\n", (2772 - c));
 	return (0);
