dff56780da03aab6318f912a7090bdc19f705bc0
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-26 11:35:25 -0700

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,11 +15,11 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-			s[0] = s[0] - 32;
-				for (j = 0; delimeters[j] != '\0'; j++)
-					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-						s[i + 1] = s[i + 1] - 32;
+	if (s[0] >= 97 && s[0] <= 122)
+	s[0] = s[0] - 32;
+	for (j = 0; delimeters[j] != '\0'; j++)
+	if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+	s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
 }
