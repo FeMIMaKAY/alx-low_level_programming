d2591c4f7c7182033cb7cdfcb1b51dab1d602c6c
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-26 11:45:25 -0700

    function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,41 +14,41 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
 	while (*(n1 + c1) != '\0')
-		c1++;
+	c1++;
 	while (*(n2 + c2) != '\0')
-		c2++;
+	c2++;
 	if (c1 >= c2)
-		bg = c1;
+	bg = c1;
 	else
-		bg = c2;
+	bg = c2;
 	if (size_r <= bg + 1)
-		return (0);
+	return (0);
 	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
+	op = dr1 + dr2 + add;
+	if (op >= 10)
+	add = op / 10;
+	else
+	add = 0;
+	if (op > 0)
+	*(r + bg) = (op % 10) + 48;
+	else
+	*(r + bg) = '0';
+	if (c1 > 0)
+	c1--, dr1 = *(n1 + c1) - 48;
+	else
+	dr1 = 0;
+	if (c2 > 0)
+	c2--, dr2 = *(n2 + c2) - 48;
+	else
+	dr2 = 0;
+	bg--, size_r--;
 	}
 	if (*(r) == '0')
-		return (r + 1);
+	return (r + 1);
 	else
-		return (r);
+	return (r);
 }
