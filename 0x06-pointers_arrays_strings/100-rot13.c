8439d13ba6ada96960b200b1179423ffe2f13a24
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-26 11:39:34 -0700

    function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,16 +13,16 @@ int j;
 char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; j < 52; j++)
-{
-if (s[i] == data1[j])
-{
-s[i] = datarot[j];
-break;
-}
-}
-}
-return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; j < 52; j++)
+	{
+	if (s[i] == data1[j])
+	{
+	s[i] = datarot[j];
+	break;
+	}
+	}
+	}
+	return (s);
 }
