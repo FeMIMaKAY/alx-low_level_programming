ec5c93ce85a7c1405ac091fddf50f2f682a48191
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-31 13:28:51 -0700

    function that locates a substring.

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,20 +13,20 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack)
 	{
-		while (*needle)
-		{
-			if (*haystack++ != *needle++)
-			{
-				break;
-			}
-		}
-		if (!*needle)
-		{
-			return (result);
-		}
-		needle = fneedle;
-		result++;
-		haystack = result;
+	while (*needle)
+	{
+	if (*haystack++ != *needle++)
+	{
+	break;
+	}
+	}
+	if (!*needle)
+	{
+	return (result);
+	}
+	needle = fneedle;
+	result++;
+	haystack = result;
 	}
 	return (0);
 }
