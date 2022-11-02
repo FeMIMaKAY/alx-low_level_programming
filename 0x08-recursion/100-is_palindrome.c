7415698a61b1f6b33e9af694262999aff0e717df
oluwafemi <makanjuolafemi17@gmail.com>
2022-11-02 12:14:35 -0700

    function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s)
 {
 	if (!*s)
 	{
-		return (0);
+	return (0);
 	}
 	return (1 + _strlen_recursion(++s));
 }
@@ -26,12 +26,12 @@ int p1(char *s, int l)
 {
 	if (l < 1)
 	{
-		return (1);
+	return (1);
 	}
 
 	if (*s == *(s + l))
 	{
-		return (p1(s + 1, l - 2));
+	return (p1(s + 1, l - 2));
 	}
 	return (0);
 }
