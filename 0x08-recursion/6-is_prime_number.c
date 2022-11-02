29ea40b1996dc17729f8c26e4d998e423ad9e37a
oluwafemi <makanjuolafemi17@gmail.com>
2022-11-02 12:12:34 -0700

    returns 1 if the input integer is a prime number,

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,15 +13,15 @@ int divisors(int n, int m)
 {
 	if (m % n == 0)
 	{
-		return (0);
+	return (0);
 	}
 	else if (m / 2 > n)
 	{
-		return (divisors(n + 2, m));
+	return (divisors(n + 2, m));
 	}
 	else
 	{
-		return (1);
+	return (1);
 	}
 }
 
@@ -35,10 +35,10 @@ int is_prime_number(int n)
 {
 	if ((!(n % 2) && n != 2) || n < 2)
 	{
-		return (0);
+	return (0);
 	}
 	else
 	{
-		return (divisors(3, n));
+	return (divisors(3, n));
 	}
 }
