8954547d50388e4b54317936edfa4c2a27843421
Oluwafemi <makanjuolafemi17@gmail.com>
2022-11-10 20:49:17 -0800

    program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,23 +14,23 @@ void _is_zero(char *argv[])
 	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
+	if (argv[1][i] != '0')
+	{
+	isn1 = 0;
+	break;
+	}
 
 	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
+	if (argv[2][i] != '0')
+	{
+	isn2 = 0;
+	break;
+	}
 
 	if (isn1 == 1 || isn2 == 1)
 	{
-		printf("0\n");
-		exit(0);
+	printf("0\n");
+	exit(0);
 	}
 }
 
@@ -46,7 +46,7 @@ char *_initialize_array(char *ar, int lar)
 	int i = 0;
 
 	for (i = 0; i < lar; i++)
-		ar[i] = '0';
+	ar[i] = '0';
 	ar[lar] = '\0';
 	return (ar);
 }
@@ -64,11 +64,11 @@ int _checknum(char *argv[], int n)
 	int ln;
 
 	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
+	if (!isdigit(argv[n][ln]))
+	{
+	printf("Error\n");
+	exit(98);
+	}
 
 	return (ln);
 }
@@ -87,39 +87,39 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
+	printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
-		printf("Error\n"), exit(98);
+	printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
-		if (i < 0)
-		{
-			if (addl > 0)
-			{
-				add = (nout[k] - '0') + addl;
-				if (add > 9)
-					nout[k - 1] = (add / 10) + '0';
-				nout[k] = (add % 10) + '0';
-			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
-		}
-		if (j < 0)
-		{
-			if (nout[0] != '0')
-				break;
-			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-		}
-		if (j >= 0)
-		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
-		}
+	if (i < 0)
+	{
+	if (addl > 0)
+	{
+	add = (nout[k] - '0') + addl;
+	if (add > 9)
+	nout[k - 1] = (add / 10) + '0';
+	nout[k] = (add % 10) + '0';
+	}
+	i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+	}
+	if (j < 0)
+	{
+	if (nout[0] != '0')
+	break;
+	lnout--;
+	free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	}
+	if (j >= 0)
+	{
+	add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+	addl = add / 10, nout[k] = (add % 10) + '0';
+	}
 	}
 	printf("%s\n", nout);
 	return (0);
