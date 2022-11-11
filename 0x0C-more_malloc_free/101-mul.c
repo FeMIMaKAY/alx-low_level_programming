a2078f9224563e1d6e3dc0007ce60c50b8a18127
Oluwafemi <makanjuolafemi17@gmail.com>
2022-11-10 20:57:07 -0800

    program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,12 +15,12 @@ int i, j;
 	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	if (argv[i][j] > 57 || argv[i][j] < 48)
+	{  printf("Error\n");
+	exit(98); }
+	}
 
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
