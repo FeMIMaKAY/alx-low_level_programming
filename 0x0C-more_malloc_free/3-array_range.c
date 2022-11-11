2b1f3c26436c429b07fac18ff6cdb63edc337560
Oluwafemi <makanjuolafemi17@gmail.com>
2022-11-10 20:45:36 -0800

    function that creates an array of integers.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,15 +15,15 @@ int *array_range(int min, int max)
 	int i;
 
 	if (min > max)
-		return (NULL);
+	return (NULL);
 
 	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 
 	if (ar == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; min <= max; i++, min++)
-		ar[i] = min;
+	ar[i] = min;
 
 	return (ar);
 }
