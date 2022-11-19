a2dddb2dea3deba3e3b9b021ffd14e62bfdb3824
Oluwafemi <makanjuolafemi17@gmail.com>
2022-11-18 20:22:25 -0800

    task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	for (i = 0; i < size; i++)
+	action(array[i]);
 }
--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,23 +15,23 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error\n");
-		exit(1);
+	printf("Error\n");
+	exit(1);
 	}
 
 	nbytes = atoi(argv[1]);
 
 	if (nbytes < 0)
 	{
-		printf("Error\n");
-		exit(2);
+	printf("Error\n");
+	exit(2);
 	}
 
 	for (i = 0; i < nbytes; i++)
 	{
-		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
-			printf(" ");
+	printf("%02x", opc[i] & 0xFF);
+	if (i != nbytes - 1)
+	printf(" ");
 	}
 
 	printf("\n");
